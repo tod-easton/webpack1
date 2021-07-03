9fdf8b4a8dd43f32a5d87766247542a7a4b42e05
Nmuta Jones <nmuta1@gmail.com>
2021-07-03 15:50:56 +0300

    Add all files for css to work in the pack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { pascalCase } from "pascal-case";
-import  themessage  from "./message.js";
+import themessage from "./message.js";
+
+import './css/coffee.css';
+import './css/colors.css';
+import './css/main.css';
 
 const title = document.getElementById("title");
 
