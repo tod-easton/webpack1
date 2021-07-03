fab7b19a094b96fb24ce6b6a17bf1648d0bde782
Nmuta Jones <nmuta1@gmail.com>
2021-07-03 18:29:05 +0300

    Adding the new css minifiers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // THIS LINE IS NEW 
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); // THIS LINE IS NEW 
+
 const path = require('path');
 
 module.exports = {
+  plugins: [new MiniCssExtractPlugin()], // THIS LINE IS NEW 
   entry: ['./src/index.js'],
   output: {
     filename: 'main.js',
@@ -10,8 +14,17 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // the `...` syntax on the next line extends the existing minimizers (i.e. `terser-webpack-plugin`), 
+      // for example, if you want to keep minifiying javascript as you were before, keep the `...` 
+      // before calling new CssMinimizerPlugin()
+      // `...`,
+      // new CssMinimizerPlugin(),
     ],
   },
 };
