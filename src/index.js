e8e66cda159ce9b210e6ed0efd4a4dc0e242bb06
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-19 11:35:28 -0700

    add image example code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { pascalCase } from "pascal-case";
 import themessage from "./message.js";
+import Spock from './Spock.png'; 
 
 import './css/coffee.css';
 import './css/colors.css';
@@ -9,4 +10,9 @@ const title = document.getElementById("title");
 
 title.innerHTML = pascalCase(themessage);
 
+   // Add the image to our existing div.
+   const myNewImage = new Image();
+   myNewImage.src = Spock;
+ 
+   element.appendChild(myNewImage);
  
